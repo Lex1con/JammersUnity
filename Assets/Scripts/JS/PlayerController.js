4162cefd2a50a36f8b68df818d7d6c494382a029
Mikkel <mikkel.hayes61@gmail.com>
2018-11-08 11:11:38 -0400

    Session 1
    Initial Commit with content from session 1

---
+++ Assets/Scripts/JS/PlayerController.js
@@ -0,0 +1,52 @@
+﻿#pragma strict
+
+private var rb: Rigidbody;
+
+public var speed = 0.0;
+
+private var count: int;
+
+public var countText: GameObject;
+countText.Find('countText');
+
+public var winText: GameObject;
+winText.Find('winText');
+
+function Start () {
+	rb = GetComponent(Rigidbody);
+
+	count = 0;
+	SetCountText();
+	winText.GetComponent(UI.Text).text = "";
+}
+
+function FixedUpdate () {
+	var moveHorizontal = Input.GetAxis("Horizontal");
+
+	var moveVertical = Input.GetAxis("Vertical");
+
+	var movement = Vector3(moveHorizontal, 0.0f, moveVertical);
+
+	rb.AddForce(movement * speed);
+}
+
+function OnTriggerEnter(other: Collider){
+
+	if(other.gameObject.CompareTag("Pick Up")){
+
+		other.gameObject.SetActive(false);
+
+		count = count + 1;
+
+		SetCountText();
+	}
+
+}
+
+function SetCountText(){
+
+	countText.GetComponent(UI.Text).text = "Count: " + count;
+	if (count >= 12){
+		winText.GetComponent(UI.Text).text = "You Win";
+	}
+}
